4d5c4e13e07769d08b452c1cf76d71f3803fea03
HyltheRambler <hylthevoidrambler@gmail.com>
2024-09-22 14:18:05 +0800

    try another one?

--- lab2/Intlist/IntList.java
+++ lab2/Intlist/IntList.java
@@ -83,7 +83,8 @@ public class IntList {
     public static IntList dcatenate(IntList A, IntList B) {
         //TODO:  fill in method
         if (A == null) {
-            return B;
+            A = B;
+            return A;
         }
         IntList ptr = A;
         while (ptr.rest != null) {
